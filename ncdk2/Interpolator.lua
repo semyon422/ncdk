1a69d9853a3279d3eb2241885c91629d3684d336
semyon422 <semyon422@users.noreply.github.com>
2024-05-01 22:30:02 +0500

    Update Interpolator.lua

--- ncdk2/Interpolator.lua
+++ ncdk2/Interpolator.lua
@@ -24,28 +24,18 @@ function Interpolator:getBaseIndex(list, index, p, ext)
 
 	local _p = list[index]
 	if p == _p or ext(p):compare(ext(_p)) and index == 1 then
-		-- skip
-	elseif ext(_p):compare(ext(p)) then
-		local next_vp = list[index + 1]
-		while next_vp do
-			if not ext(p):compare(ext(next_vp)) then
-				index = index + 1
-				next_vp = list[index + 1]
-			else
-				break
-			end
+		return index
+	end
+
+	if ext(_p):compare(ext(p)) then
+		while list[index + 1] and not ext(p):compare(ext(list[index + 1])) do
+			index = index + 1
 		end
-	elseif ext(p):compare(ext(_p)) then
+		return index
+	end
+
+	while list[index] and ext(p):compare(ext(list[index])) do
 		index = index - 1
-		local prev_vp = list[index]
-		while prev_vp do
-			if ext(p):compare(ext(prev_vp)) then
-				index = index - 1
-				prev_vp = list[index]
-			else
-				break
-			end
-		end
 	end
 
 	return math.max(index, 1)
