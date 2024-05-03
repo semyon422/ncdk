d30355797083788bf8c53da7bfc97db47ae011c6
semyon422 <semyon422@users.noreply.github.com>
2024-05-03 22:29:09 +0500

    Update Chart.lua

--- ncdk2/Chart.lua
+++ ncdk2/Chart.lua
@@ -1,11 +1,10 @@
 local class = require("class")
 local InputMode = require("ncdk.InputMode")
 local ResourceList = require("ncdk.ResourceList")
-local Layer = require("ncdk2.layers.Layer")
 
 ---@class ncdk2.Chart
 ---@operator call: ncdk2.Chart
----@field layers ncdk2.Layer[]
+---@field layers {[string]: ncdk2.Layer}
 local Chart = class()
 
 function Chart:new()
@@ -17,36 +16,18 @@ end
 ---@return function
 function Chart:getNotesIterator()
 	return coroutine.wrap(function()
-		for layerIndex, layer in ipairs(self.layers) do
+		for layerIndex, layer in pairs(self.layers) do
 			for inputType, r in pairs(layer.notes.data) do
 				for inputIndex, notes in pairs(r) do
-					coroutine.yield(notes, inputType, inputIndex, layerIndex)
+					coroutine.yield(notes, inputType, inputIndex, layerIndex, layer)
 				end
 			end
 		end
 	end)
 end
 
----@return function
----@return table
----@return number
-function Chart:ilayers()
-	return ipairs(self.layers)
-end
-
----@param i number
----@return ncdk2.Layer
-function Chart:getLayer(i)
-	local layers = self.layers
-	if not layers[i] then
-		assert(i == #layers + 1)
-		layers[i] = Layer()
-	end
-	return layers[i]
-end
-
 function Chart:compute()
-	for _, layer in ipairs(self.layers) do
+	for _, layer in pairs(self.layers) do
 		layer:compute()
 	end
 end
